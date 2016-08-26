2d9663543e8224bbb9f9107a0b89074fe58237d0
John Verrone <john.verrone@gmail.com>
2016-08-25 22:53:56 -0400

    routed data requests through api-auth pipeline

--- web/router.ex
+++ web/router.ex
@@ -2,15 +2,15 @@ defmodule RocketleaguePhoenix.Router do
   use RocketleaguePhoenix.Web, :router
 
   pipeline :api do
-    plug :accepts, ["json-api"]
-    plug JaSerializer.ContentTypeNegotiation
-    plug JaSerializer.Deserializer
+    plug :accepts, ["json", "json-api"]
   end
 
   pipeline :api_auth do
     plug :accepts, ["json-api"]
     plug Guardian.Plug.VerifyHeader
     plug Guardian.Plug.LoadResource
+    plug JaSerializer.ContentTypeNegotiation
+    plug JaSerializer.Deserializer
   end
 
   scope "/api", RocketleaguePhoenix do
@@ -19,15 +19,15 @@ defmodule RocketleaguePhoenix.Router do
     post "/register", RegistrationController, :create
     post "/token", SessionController, :create, as: :login
 
-    resources "/matches", MatchController, except: [:new, :edit]
-    resources "/players", PlayerController, except: [:new, :edit]
-    resources "/session", SessionController, only: [:index]
-    resources "/teams", TeamController, except: [:new, :edit]
   end
 
   scope "/api", RocketleaguePhoenix do
     pipe_through :api_auth
 
     get "/user/current", UserController, :current
+    resources "/matches", MatchController, except: [:new, :edit]
+    resources "/players", PlayerController, except: [:new, :edit]
+    resources "/session", SessionController, only: [:index]
+    resources "/teams", TeamController, except: [:new, :edit]
   end
 end
